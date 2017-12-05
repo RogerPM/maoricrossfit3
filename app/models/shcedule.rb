6d4c8b2b2f87a8208ca9562f7e015483044cba92
Roger <robafiel@hotmail.com>
2017-12-05 11:42:10 -0500

    shcedule

---
+++ app/models/shcedule.rb
@@ -0,0 +1,4 @@
+class Shcedule < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :service
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :shcedules
+  resources :shcedules
   resources :checks
   resources :subscriptions
   resources :memberships
