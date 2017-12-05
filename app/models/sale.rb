628fbd4f2894ec14acdf81afb0610eb481b661ed
Roger <robafiel@hotmail.com>
2017-12-05 11:20:35 -0500

    user y sale

--- app/models/sale.rb
+++
@@ -1,4 +0,0 @@
-class Sale < ActiveRecord::Base
-  belongs_to :client
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sales
   resources :products
   resources :clients
   # The priority is based upon order of creation: first created -> highest priority.
